4dd3fe3288ab862c053b4fbb7c0c4c28a160bd36
David Powers <dpowers@macsales.com>
2021-07-08 14:45:44 -0500

    change case

--- backend/Application/Actions/Account/Login.cs
+++ backend/Application/Actions/Account/Login.cs
@@ -19,7 +19,7 @@ namespace Application.Actions.Account
     {
         public class Command : IRequest<string>
         {
-            public string UserName { get; set; }
+            public string Username { get; set; }
             public string Password { get; set; }
             public string IpAddress { get; set; }
         }
