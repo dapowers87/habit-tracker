ce844435bfb7c167125ddfc7f18d3db4ae8705d5
David Powers <dpowers@macsales.com>
2021-07-12 15:17:21 -0500

    add update logic

--- client/src/pages/TrackerWindow/components/CreateUpdateModal.tsx
+++ client/src/pages/TrackerWindow/components/CreateUpdateModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import SemanticDatepicker from 'react-semantic-ui-datepickers';
 import { toast } from 'react-toastify';
 import { Form, Modal } from 'semantic-ui-react';
@@ -6,7 +6,7 @@ import agent from '../../../api/agent';
 import { AppContext, IInitialState, types } from '../../../store';
 import ITrackerWindow from '../../../types/ITrackerWindow';
 
-const CreateUpdateModal: React.FC<{open: boolean, setOpen: (newVal: boolean) => void}> = ({open, setOpen}) => {
+const CreateUpdateModal: React.FC<{window?: ITrackerWindow, open: boolean, setOpen: (newVal: boolean) => void}> = ({window, open, setOpen}) => {
 
     const { state, dispatch } = useContext(AppContext);
     const { Windows } = state as IInitialState;
@@ -16,35 +16,59 @@ const CreateUpdateModal: React.FC<{open: boolean, setOpen: (newVal: boolean) => 
     const [numberOfDays, setNumberOfDays] = useState<number>(100);
     const [numberOfCheatDays, setNumberOfCheatDays] = useState<number>(10);
 
+    useEffect(() => {
+        if(window) {
+            setWindowName(window.windowName);
+            setStartDate(window.startDate);
+            setNumberOfDays(window.numberOfDays);
+            setNumberOfCheatDays(window.numberOfCheatDays);
+        }
+    }, [window]);
+
     const handleCreateOrUpdate = async () => {
-        const window = {
+        const clearModal = () => {
+            setWindowName('');
+            setStartDate(new Date());
+            setNumberOfDays(100);
+            setNumberOfCheatDays(10);
+        };
+
+        let windowToSend = {
             windowName,
             startDate,
             numberOfCheatDays,
             numberOfDays
         } as ITrackerWindow;
 
-        const result = await agent.Window.create(window);
-
-        if(result) {
-            toast.success(`Window '${windowName}' Created`);
-
-            const newWindow = {...window, windowId: result, numberOfCheatDaysUsed: 0 } as ITrackerWindow;
-
-            if(Windows) {
-                dispatch({type: types.SETWINDOWS, Windows: [newWindow, ...Windows]});
-            } else {
-                dispatch({type: types.SETWINDOWS, Windows: [newWindow]});
+        if(window) {
+            windowToSend.windowId = window.windowId;
+            const result = await agent.Window.update(windowToSend); 
+            if(result) {
+                toast.success("Window updated");
+                clearModal();
+                setOpen(false);
             }
-
-            setWindowName('');
-            setStartDate(new Date());
-            setNumberOfDays(100);
-            setNumberOfCheatDays(10);
-            
-            setOpen(false);
         } else {
-            toast.error("Error creating window");
+
+            const result = await agent.Window.create(windowToSend);
+
+            if(result) {
+                toast.success(`Window '${windowName}' Created`);
+
+                const newWindow = {...windowToSend, windowId: result, numberOfCheatDaysUsed: 0 } as ITrackerWindow;
+
+                if(Windows) {
+                    dispatch({type: types.SETWINDOWS, Windows: [newWindow, ...Windows]});
+                } else {
+                    dispatch({type: types.SETWINDOWS, Windows: [newWindow]});
+                }
+
+                clearModal();
+                
+                setOpen(false);
+            } else {
+                toast.error("Error creating window");
+            }
         }
     }
 
@@ -60,7 +84,7 @@ const CreateUpdateModal: React.FC<{open: boolean, setOpen: (newVal: boolean) => 
                             onChange={(_, value) => setNumberOfDays(parseInt(value.value))}/> 
                         <Form.Input min={0} type='number' fluid label="Number of Cheat Days" placeholder='10' value={numberOfCheatDays}
                             onChange={(_, value) => setNumberOfCheatDays(parseInt(value.value))}/> 
-                        <Form.Button positive onClick={handleCreateOrUpdate}>Create</Form.Button>
+                        <Form.Button positive onClick={handleCreateOrUpdate}>{window ? 'Update' : 'Create'}</Form.Button>
                     </Form>
                 </Modal.Content>
             </Modal>
