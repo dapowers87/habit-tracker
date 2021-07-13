df3415a2a16f80cb6a2c5127362af6b23224e88b
David Powers <dpowers@macsales.com>
2021-07-13 11:43:08 -0500

    add logout

--- client/src/pages/TrackerWindow/TrackerWindow.tsx
+++ client/src/pages/TrackerWindow/TrackerWindow.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useCallback, useContext, useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom';
 import { Button, Card, Grid } from 'semantic-ui-react';
 import agent from '../../api/agent';
 import { AppContext, IInitialState, types } from '../../store';
@@ -12,6 +13,8 @@ const TrackerWindow: React.FC = () => {
     const { Windows } = state as IInitialState;
 
     const [showCreateEditModal, setShowCreateEditModal] = useState<boolean>(false);
+
+    let history = useHistory();
 
     const loadWindows = useCallback(async () => {
         const windows = await agent.Window.getAll();
@@ -27,12 +30,18 @@ const TrackerWindow: React.FC = () => {
         return <div/>
     }
     
+    const LogOut = () => {
+        localStorage.removeItem('jwt');
+        window.location.reload();
+    }
+
     return (
         <Fragment>
             <Grid>
                 <Grid.Column textAlign='center'>
                     <Grid.Row>
                         <Button icon='plus' content='New Habit' style={{marginTop: '10px'}} positive onClick={() => setShowCreateEditModal(true)}/>
+                        <Button floated='right' icon='logout' content='Log Out' style={{marginTop: '10px', marginRight: '10px'}} onClick={LogOut}/>
                     </Grid.Row>
                 </Grid.Column>
             </Grid>
